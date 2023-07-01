4516b98c98fe154f2a2b2d237360fd256510e82c
박세윤 <yun12343@naver.com>
2023-07-01 19:32:05 +0900

    Section 2

---
+++ src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,30 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+
+        this.name = name;
+    }
+}
